9eae7da9837d366ae4b3139edf5f87ecb0b317f1
SVivdich02 <vvv-sonya@mail.ru>
2022-06-06 00:41:48 +0300

    test simple application with frame and panel

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException, NoSuchFieldException {
 
-        MyFrameGenerated app = new MyFrameGenerated(); //1
+        MyFrameGenerated app = new MyFrameGenerated();
         Rectangle r = new Rectangle();
         r.height = 500;
         r.x = 200;
         r.y = 200;
         r.width = 500;
-        app.setBounds(r); //2
-        app.setVisible(true); //3
+        app.setBounds(r);
+        app.setVisible(true);
 
-
-        /*
         MyPanelGenerated panel = new MyPanelGenerated();
-        MyButtonGenerated button = new MyButtonGenerated();
+        panel.setBackground(Color.ORANGE);
 
-        panel.setBackground(Color.GREEN);
-        button.setText("just text");
-        panel.add("hello", button);
+        app.setTitle("У меня есть двойник?");
         app.add(panel);
-
-         */
-
 
         int correctlySize = GlobalList.list.size();
         for (int i = 0; i < correctlySize; i++)
         {
             System.out.println(GlobalList.list.get(i).getMethodName() + " " + GlobalList.list.get(i).getClassName() + " " + GlobalList.list.get(i).getParamValuesMap());
         }
-        System.out.println(GlobalList.list.size());
+        System.out.println();
 
-        GlobalList.runList(correctlySize);
+        GlobalList.runList(GlobalList.list.size());
     }
 }
