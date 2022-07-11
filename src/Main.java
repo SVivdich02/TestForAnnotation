36d31e157a952219533332f5ad3b871cadb88c06
SVivdich02 <vvv-sonya@mail.ru>
2022-07-11 18:08:21 +0300

    complicated the application

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import org.example.annotation.GlobalList;
-import org.example.annotation.MyButtonGenerated;
-import org.example.annotation.MyFrameGenerated;
-import org.example.annotation.MyPanelGenerated;
+import org.example.annotation.*;
 
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
@@ -9,7 +6,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException, NoSuchFieldException {
-
         MyFrameGenerated app = new MyFrameGenerated();
         Rectangle r = new Rectangle();
         r.height = 500;
@@ -21,6 +17,17 @@ public class Main {
 
         MyPanelGenerated panel = new MyPanelGenerated();
         panel.setBackground(Color.ORANGE);
+
+        MyButtonGenerated b = new MyButtonGenerated();
+        Rectangle r2 = new Rectangle();
+        r2.height = 50;
+        r2.x = 300;
+        r2.y = 300;
+        r2.width = 50;
+        b.setBounds(r2);
+        b.setVisible(true);
+
+        panel.add(b);
 
         app.setTitle("У меня есть двойник?");
         app.add(panel);
